fbf4b1329e384baabe6792ad64c1e220afbe420d
Aman Jain <amanjain2k26@gmail.com>
2026-08-03 08:09:38 +0530

    Time: 0 ms (100.00%) | Memory: 52.5 MB (95.28%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -4,23 +4,12 @@
  */
 var check = function(arr) {
     let count = 0;
-    let isMore = false;
 
-    if (arr[arr.length - 1] > arr[0]) {
-        count++;
-    }
-
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            count = count + 1;
+    for (let i = 0; i < arr.length; i++) {
+        let next = (i + 1) % arr.length;
+        if (arr[i] > arr[next]) {
+            count++;
         }
     }
-
-    if (count === 0 || count === 1) {
-        isMore = true;
-    } else {
-        isMore = false;
-    }
-
-    return isMore;
+    return count <= 1
 };
