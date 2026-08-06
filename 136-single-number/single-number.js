eca7c6c933ab04451d93660de54bc91df2375fbe
Aman Jain <amanjain2k26@gmail.com>
2026-08-06 08:07:40 +0530

    Time: 0 ms (100.00%) | Memory: 56.3 MB (56.01%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,21 +3,10 @@
  * @return {number}
  */
 var singleNumber = function(arr) {
-    let map = new Map();
-
-    for(let i = 0; i < arr.length; i++){
-        if(map.has(arr[i])){
-            map.set(arr[i], (map.get(arr[i])) + 1);
-        }else{
-            map.set(arr[i], 1)
-        }
-    }
-
     let ans = 0;
-    for(let [key, value] of map){
-        if(value === 1){
-            ans = key;
-        }
+
+    for(let num of arr){
+        ans = ans ^ num;
     }
 
     return ans;
