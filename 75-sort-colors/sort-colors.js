14619ad0a87a1ccf4ba2e3ee8081b455a9866980
Aman Jain <amanjain2k26@gmail.com>
2026-08-09 14:29:14 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (16.15%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,33 +3,24 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(arr) {
-    // Second approch
-    let count1 = 0;
-    let count2 = 0;
-    let count3 = 0;
+    //3rd and optimized approch
+    let low = 0 
+    let mid = 0;
+    let heigh = arr.length - 1;
 
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] === 0){
-            count1++;
-        }else if(arr[i] === 1){
-            count2++;
+
+    while(mid <= heigh){
+        if(arr[mid] === 0){
+            [arr[low], arr[mid]] = [arr[mid], arr[low]];
+            low++;
+            mid++;
+        }else if(arr[mid] === 1){
+            mid++
         }else{
-            count3++;
+            [arr[mid], arr[heigh]] = [arr[heigh], arr[mid]];
+            heigh--;
         }
     }
-
-    for(let j = 0; j < count1; j++){
-        arr[j] = 0
-    }
-
-    for(let k = count1; k < count1 + count2; k++){
-        arr[k] = 1
-    }
-
-    for(let l = count1 + count2; l < arr.length; l++){
-        arr[l] = 2
-    }
-
 
     return arr;
 };
