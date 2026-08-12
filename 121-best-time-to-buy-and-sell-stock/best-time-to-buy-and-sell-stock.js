1aefe6b04415d1ea791efc55de335abf8cd191fc
Aman Jain <amanjain2k26@gmail.com>
2026-08-12 09:02:31 +0530

    Time: 2 ms (80.31%) | Memory: 65.5 MB (25.09%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    let minPrice = prices[0];
+
+    for(let i = 0; i < prices.length; i++){
+        if(prices[i] < minPrice){
+            minPrice = prices[i]
+        }
+
+        let profit = prices[i] - minPrice;
+        maxProfit = Math.max(maxProfit, profit)
+    }
+
+    return maxProfit;
+};
