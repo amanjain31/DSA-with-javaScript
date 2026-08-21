5266286da78ff87824f3110bc64615647725d8e4
Aman Jain <amanjain2k26@gmail.com>
2026-08-21 17:37:27 +0530

    Time: 4 ms (73.51%) | Memory: 60.3 MB (86.47%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,23 @@
+var romanToInt = function(s) {
+    const value = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (value[s[i]] < value[s[i + 1]]) {
+            result -= value[s[i]];
+        } else {
+            result += value[s[i]];
+        }
+    }
+
+    return result;
+};
