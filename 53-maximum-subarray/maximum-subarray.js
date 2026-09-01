36c6277a6b56589aef2fff77e268ebe26ac04f18
Aman Jain <amanjain2k26@gmail.com>
2026-09-01 08:52:37 +0530

    Time: 4 ms (22.27%) | Memory: 63.7 MB (67.69%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -3,6 +3,18 @@
  * @return {number}
  */
 var maxSubArray = function(arr) {
+
+    // For this question, we basically follow three steps using **Kadane's Algorithm.
+
+    // Step 1: We iterate through the array and add each `i`th element to the `sum`.
+
+    // Step 2: After updating the `sum`, we compare it with our `max` value. If the current `sum` is greater than `max`, we update `max` with the current `sum`.
+    
+    // Step 3: Finally, we check whether the `sum` has become less than `0`. If it has, we reset `sum` to `0` because a negative sum will not help us create a larger subarray sum in the future.
+
+    // This is the basic idea behind Kadane's Algorithm for finding the maximum subarray sum.
+
+ 
     let sum = 0;
     let maxi = -Infinity
 
