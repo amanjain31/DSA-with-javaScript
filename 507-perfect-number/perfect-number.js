7228eb1b1c7df2815e16c3373764485c8198f89d
Aman Jain <amanjain2k26@gmail.com>
2026-09-01 09:48:44 +0530

    Time: 2098 ms (12.08%) | Memory: 55.5 MB (19.13%) - LeetSync

---
+++ 507-perfect-number/perfect-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(n) {
+    let sum = 0;
+    for(let i = 1; i <= n - 1; i++){
+        if(n % i === 0){
+            sum += i;
+        }
+    }
+
+    return sum === n;
+};
