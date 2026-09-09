b2558d221e0611d2e9478add29a64414c796d6bc
Aman Jain <amanjain2k26@gmail.com>
2026-09-09 07:59:33 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (98.87%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(arr) {
+    let sum = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+        arr[i] = sum;
+    }
+
+    return arr;
+};
