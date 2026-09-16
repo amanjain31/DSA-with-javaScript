9719ad6fae7f8137d1c0f1f598f4216b4ba3cb5e
Aman Jain <amanjain2k26@gmail.com>
2026-09-16 13:30:44 +0530

    Time: 15 ms (36.36%) | Memory: 53.6 MB (100.00%) - LeetSync

---
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfSets = function(n, k) {
+    const MOD = 1000000007;
+
+    // We need C(n + k - 1, 2k)
+    const N = n + k - 1;
+    const R = 2 * k;
+
+    let dp = new Array(R + 1).fill(0);
+    dp[0] = 1;
+
+    for (let i = 1; i <= N; i++) {
+        for (let j = Math.min(i, R); j >= 1; j--) {
+            dp[j] = (dp[j] + dp[j - 1]) % MOD;
+        }
+    }
+
+    return dp[R];
+};
