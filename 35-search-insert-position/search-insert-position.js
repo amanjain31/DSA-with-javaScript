bbdd195c3b2c372d071ee6b5d4110cd0341fd24c
Aman Jain <amanjain2k26@gmail.com>
2026-09-17 09:01:54 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (98.11%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(arr, target) {
+    let start = 0;
+    let end = arr.length - 1;
+
+    while(start <= end){
+        let mid = Math.floor((start + end) / 2);
+
+        if(arr[mid] === target){
+            return mid;
+        }
+        else if(arr[mid] < target){
+            start = mid + 1;
+        }else{
+            end = mid - 1;
+        }
+    }
+
+    return start;
+};
